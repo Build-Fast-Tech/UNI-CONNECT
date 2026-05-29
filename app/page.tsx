a3e3d65c5199d6eef0d45883e8fb7574e1084a8f
abdulahxf90 <abdullah.xf90@gmail.com>
2026-05-29 14:34:58 +0500

    fix: mark HomePage as client component to use useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { MarketingNav }        from "@/components/layout/MarketingNav";
 import { Footer }              from "@/components/layout/Footer";
 import { HeroSection }        from "@/components/landing/HeroSection";
